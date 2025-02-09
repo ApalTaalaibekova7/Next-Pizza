dc65b341471f608e36db38984a9dd7b476de1beb
ApalTaalaibekova7 <apaltaalaibekova7@gmail.com>
2025-02-09 06:02:17 +0300

    add header

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,12 @@
-import { Button } from "@/components/ui/button";
-import Image from "next/image";
+import { Container, Title } from "@/components/shared";
+
 
 export default function Home() {
   return (
-  <div>
-    <h1>Home
-<Button variant='outline'>Корзина</Button>
-    </h1>
-  </div>
+  <Container className="mt-10">
+   <Title text="Все пиццы" size="lg" className="font-extrabold" />
+
+  </Container>
+
   );
 }
